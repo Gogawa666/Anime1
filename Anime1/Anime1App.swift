72cb97863ac2b947d85b0071af748ce3fc4eda06
Gogawa666 <gogawa@lcs.on.ca>
2021-02-23 14:46:22 -0500

    Initial Commit

---
+++ Anime1/Anime1App.swift
@@ -0,0 +1,17 @@
+//
+//  Anime1App.swift
+//  Anime1
+//
+//  Created by Gaku Ogawa on 2021/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Anime1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anime1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Anime1
+//
+//  Created by Gaku Ogawa on 2021/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
